9bbbf1d32fe80afbf330795bb1c2c67642907cac
Viet Hung <anhtrac6678@gmail.com>
2026-05-25 18:17:01 +0700

    fix path cloud + faiss

--- src/config.py
+++ src/config.py
@@ -1,6 +1,9 @@
 import os
 import streamlit as st
 
+# ======================
+# OPENAI KEY (Cloud + Local safe)
+# ======================
 def get_openai_key():
     try:
         key = st.secrets.get("OPENAI_API_KEY")
@@ -11,10 +14,13 @@ def get_openai_key():
 
     return os.getenv("OPENAI_API_KEY")
 
+
 OPENAI_API_KEY = get_openai_key()
 
 if not OPENAI_API_KEY:
-    raise ValueError("❌ OPENAI_API_KEY NOT FOUND (Streamlit Secrets or Env)")
+    raise ValueError("❌ OPENAI_API_KEY NOT FOUND")
+
+
 # ======================
 # Models
 # ======================
@@ -22,18 +28,21 @@ EMBEDDING_MODEL = "text-embedding-3-small"
 EMBEDDING_MODEL_LARGE = "text-embedding-3-large"
 LLM_MODEL = "gpt-4o-mini"
 
+
 # ======================
-# Base path
+# ROOT PATH (FIX STREAMLIT CLOUD)
 # ======================
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 # ======================
 # Database
 # ======================
 DB_PATH = os.path.join(BASE_DIR, "DB_code", "DB_Shop.db")
 
+
 # ======================
-# FAISS paths
+# FAISS paths (FIXED)
 # ======================
 FAISS_Products = os.path.join(BASE_DIR, "vector_store", "Faiss_Product")
 FAISS_Policies = os.path.join(BASE_DIR, "vector_store", "Faiss_Policies")
